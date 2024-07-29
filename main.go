3b5b39c8be9a0e92d86b3f565002a8098e5d0181
dovban <dovban@mdis.ru>
2024-07-29 10:38:32 +0300

    Структуры и ООП

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
+	"time"
 )
 
 type account struct {
@@ -11,39 +14,63 @@ type account struct {
 	url      string
 }
 
+type accountWhithTime struct {
+	createAt time.Time
+	updateAt time.Time
+	account
+}
+
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	arr := []rune("asdfghjklpiuyrewqzxcvbnm")
+	res := make([]rune, n)
+	for i := 0; i < n; i++ {
+		res[i] = arr[rand.IntN(len(arr))]
+	}
+	acc.password = string(res)
+}
+
+func newAccount(login, password, urlString string) (*accountWhithTime, error) {
+	if login == "" {
+		return nil, errors.New("LOGIN_EMPTY_STING")
+	}
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAcc := &accountWhithTime{
+		createAt: time.Now(), updateAt: time.Now(), account: account{login: login, password: password, url: urlString},
+	}
+
+	if password == "" {
+		newAcc.account.generatePassword(10)
+	}
+
+	return newAcc, nil
+}
 func main() {
-
-	fmt.Println(generatePassword(10))
-
 	login := promptData("Введите логин: ")
 	password := promptData("Введите пароль: ")
 	url := promptData("Введите URL: ")
 
-	myAccount := account{
-		login: login, url: url, password: password,
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	outputPassword(&myAccount)
+	myAccount.outputPassword()
 
 }
 
 func promptData(prompt string) string {
 	fmt.Print(prompt)
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
-}
-
-func outputPassword(acc *account) {
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func generatePassword(n int) string {
-	arr := []rune("asdfghjklpiuyrewqzxcvbnm")
-	res := make([]rune, n)
-	for i := 0; i < n; i++ {
-		res[i] = arr[rand.IntN(len(arr))]
-	}
-
-	return string(res)
 }
