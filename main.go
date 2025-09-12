3d69b83357793642b02c95c4e48c6b4dd7f57154
Довбан Андрей Сергеевич <dovban@mdis.ru>
2025-09-12 11:40:39 +0300

    fix

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"math/rand/v2"
+	"strings"
+)
+
+type Account struct {
+	Login    string
+	Password string
+	Url      string
+}
+
+func (acc *Account) output() {
+	fmt.Println("Login: ", acc.Login)
+	fmt.Println("Password: ", acc.Password)
+	fmt.Println("Url: ", acc.Url)
+	fmt.Println()
+
+}
+
+func (acc *Account) generatePassword(n int) {
+	runes := []rune("qwertyuiopasdfghjklzxcvbnm123456789ASDFGHJKLZXCVBNMQWERTYUIOP")
+	pass := make([]string, 0, n)
+
+	for range n {
+		pass = append(pass, string(runes[rand.IntN(len(runes))]))
+	}
+	acc.Password = strings.Join(pass, "")
+
+}
+
 func main() {
+	var data Account
+	var data2 Account
+
+	promtData(&data)
+	// promtData(&data2)
+
+	data.output()
+	data2.output()
+
+}
+
+func promtData(acc *Account) {
+
+	fmt.Println("----------------------")
+	fmt.Println("Введите логин")
+	fmt.Scan(&acc.Login)
+	fmt.Println("Генерация пароля")
+	acc.generatePassword(10)
+	fmt.Println("Введите адрес")
+	fmt.Scan(&acc.Url)
+	fmt.Println("----------------------")
 
 }
